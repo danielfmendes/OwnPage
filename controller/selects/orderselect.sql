74ec2ca9f6e23c0fb1bce497ce6a78146dbe1a24
Daniel Freire Mendes <freiremendesdaniel2002@gmail.com>
2026-06-13 14:54:59 +0100

    removes Go controller and renames api-controller to controller

--- controller/selects/orderselect.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM (SELECT o.id                              AS order_id,
-             o.order_date,
-             o.project_id,
-             o.customer_id,
-             CONCAT(c.first_name, ' ', c.name) AS customer_name,
-             c.email                           AS customer_email
-      FROM orders o
-               JOIN customers c ON o.customer_id = c.id) AS sub
